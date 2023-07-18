cd762fb77432f4d338d634d1c5116fe3eb81d5cf
Wasim <er.wasim339@gmail.com>
2023-07-18 22:11:47 +0530

    homepage added

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const features = [
+  {
+    title: "Free Shipping",
+    img: "src/images/girl",
+  },
+  {
+    title: "24/7 Support",
+    img: "src/images/girl",
+  },
+  {
+    title: "Promotion",
+    img: "src/images/girl",
+  },
+  {
+    title: "Save Money",
+    img: "src/images/girl",
+  },
+  {
+    title: "Online Order",
+    img: "src/images/girl",
+  },
+];
+
+const featureContainer = document.querySelector("features");
